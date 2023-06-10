1a9aff43a1a4f2252981313770c63e0fd25c03b8
Haa-meem <haameemalhamra@gmail.com>
2023-06-10 20:08:29 +0600

    Delete action for selected Classes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.scmnlyp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -55,6 +55,13 @@ async function run() {
             const classItem = req.body;
             console.log(classItem);
             const result = await enrolledCollection.insertOne(classItem);
+            res.send(result);
+        })
+
+        app.delete('/enrolled/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await enrolledCollection.deleteOne(query);
             res.send(result);
         })
 
