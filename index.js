14a9b3b61d1dd13854f110b6ad66c4ee4eda9f23
Haa-meem <haameemalhamra@gmail.com>
2023-06-11 01:16:18 +0600

    User to Admin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const jwt=require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -32,14 +33,31 @@ async function run() {
         const enrolledCollection = client.db("linguaDb").collection("enrolled");
 
         //users api
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user.email }
-            const existingUser = await usersCollection.findOne(query);         
+            const existingUser = await usersCollection.findOne(query);
             if (existingUser) {
                 return res.send({ message: 'user already exists' })
             }
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
             res.send(result);
         })
 
