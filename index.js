2b6b053f594e0749587671ed0fb85b20e0e13dae
Haa-meem <haameemalhamra@gmail.com>
2023-06-12 17:51:02 +0600

    vercel

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        //await client.connect();
 
         const usersCollection = client.db("linguaDb").collection("users");
         const instructorsCollection = client.db("linguaDb").collection("instructors");
