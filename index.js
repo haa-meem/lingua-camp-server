58b0079768314d9e7aefdc10c633141d348680fe
Haa-meem <haameemalhamra@gmail.com>
2023-06-12 12:59:02 +0600

    make instructor

--- index.js
+++ index.js
@@ -109,6 +109,19 @@ async function run() {
             res.send(result);
         })
 
+        //instructor
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'instructor'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
         //instructors api
         app.get('/instructors', async (req, res) => {
             const result = await instructorsCollection.find().toArray();
